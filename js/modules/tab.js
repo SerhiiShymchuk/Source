900e1cb5461a23efc30f63a6993701ee0a7d2884
SerhiiShymchuk <shymaser@gmail.com>
2021-04-11 21:31:22 +0300

    first commit

---
+++ js/modules/tab.js
@@ -0,0 +1,39 @@
+const tabs = (headerSelector, tabSelectors, contentSelectors, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelectors),
+        content = document.querySelectorAll(contentSelectors);
+
+    function hideTabContent() {
+        content.forEach(el => {
+            el.style.display = 'none'
+        })
+        tab.forEach(el => {
+            el.classList.remove(activeClass)
+        })
+    }
+
+    function showTabContent(index = 0) {
+        content[index].style.display = 'block'
+        tab[index].classList.add(activeClass)
+    }
+
+    hideTabContent()
+    showTabContent()
+
+    header.addEventListener('click', (e) => {
+        const target = e.target
+        if (target &&
+                (target.classList.contains(tabSelectors.slice(1)) || 
+                target.parentElement.classList.contains(tabSelectors.slice(1)))) {
+            tab.forEach((el, index) => {
+                if (target == el || target.parentElement == el) {
+                    hideTabContent()
+                    showTabContent(index)
+                }
+            })
+        }
+    })
+
+}
+
+export default tabs
